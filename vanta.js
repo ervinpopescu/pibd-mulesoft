7a1e49f195d7b15b4149260ad4e1671e08d7e332
ervinpopescu <ervin.popescu10@gmail.com>
2022-08-27 16:13:44 +0300

    Added files

---
+++ vanta.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', () => {
+    VANTA.NET({
+        el: "#vanta",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        backgroundColor: 0x1d1d2d
+    })
+})
